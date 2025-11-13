767d541e7eac77bcdf1d62e1828f5d832c9e648f
catsocute <nguyenlenhat205@gmail.com>
2025-11-13 23:23:09 +0700

    delete ai_module

--- src/main/java/com/se/hub/modules/ai/service/api/AiChatService.java
+++
@@ -1,8 +0,0 @@
-package com.se.hub.modules.ai.service.api;
-
-import com.se.hub.modules.ai.dto.request.AiChatRequest;
-import com.se.hub.modules.ai.dto.response.AiChatResponse;
-
-public interface AiChatService {
-    public AiChatResponse chat(AiChatRequest request);
-}
